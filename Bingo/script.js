4e38d18002f6e43f916e0539a5011fa0caf172b4
Dejmenek <dejmen14567@gmail.com>
2021-11-29 23:18:54 +0100

    Last commit

--- Bingo/script.js
+++ Bingo/script.js
@@ -6,13 +6,13 @@ var bitmap = [
     [0,0,0,0,0]
 ]
 
-var li2 = [];
 
 function random_number(){
     return Math.floor(Math.random()*75)+1;
 }
 
 function start(){
+    document.getElementById("wylacz").disabled = false;
     var li = [];
     for(var i = 0; i < 25; i++)
     {
@@ -25,29 +25,34 @@ function start(){
     }
 }
 
+var li2 = []; //used numbers
 function los(){
-    var num = random_number();
-    while(li2.includes(num)){
-        num = random_number();
+    if(li2.length < 75){
+        var num = random_number();
+        while(li2.includes(num)){
+            num = random_number();
+        }
+        li2.push(num);
+        return num;
     }
-    li2.push(num);
-    return num;
 }
 
 function change_circles(){
-    let a = los();
-    let r = document.getElementById("red");
-    let g = document.getElementById("green");
-    let y = document.getElementById("yellow");
-    y.innerHTML = g.innerHTML;
-    g.innerHTML = r.innerHTML;  
-    r.innerHTML = a;
+    if(li2.length < 75){
+        let a = los();
+        let r = document.getElementById("red");
+        let g = document.getElementById("green");
+        let y = document.getElementById("yellow");
+        y.innerHTML = g.innerHTML;
+        g.innerHTML = r.innerHTML;  
+        r.innerHTML = a;
+    }
 }
 
 function change_bg(n){
     var element = document.getElementById("c" + n);
-    var x = Math.floor(n/5);
-    var y = n % 5;
+    var x = Math.floor(n/5); //
+    var y = n % 5; //
     if(element.style.background == "" || element.style.background == "white"){
         element.style.background = "red";
         bitmap[x][y] = 1;
@@ -63,22 +68,37 @@ function change_bg(n){
 function check_win(){
     for(var i = 0; i < bitmap.length; i++){
         var exist = 0;
-        for (var j = 0; j < 5; j++){
-            if(bitmap[i][j] == 1 || bitmap[j][i] == 1 || bitmap[i+1][j+1] == 1){
+        for (var j = 0; j < bitmap[i].length; j++){
+            if(bitmap[i].includes(0) == false){ //check rows
+                exist += 1;
+                console.log(exist);
+            }
+            else if (bitmap[j][i] == 1){ //check columns
                 exist += 1;
                 console.log(exist);
             }
         }
         if(exist == 5){
-            alert("Wygrales");
-            //win();
-            exist = 0;
+            win();
+            clear_bitmap();
             break;
         }
     }
 }
 
 function win(){
-    document.getElementsById("win").innerHTML = "Wygrałeś";
+    document.getElementById("win").innerHTML = "Wygrałeś";
     document.getElementById("table").style.display = "none";
+}
+
+function restart(){
+    window.location.reload();
+}
+
+function clear_bitmap() {
+    for(var i = 0; i < bitmap.length; i++){
+        for(var j = 0; j < bitmap[i].length; j++){
+            bitmap[i][j] = 0;
+        }
+    }
 }
