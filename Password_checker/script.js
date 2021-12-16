b040005838cd30fb3dd28d4a4a1c4744ebda5a41
Dejmenek <dejmen14567@gmail.com>
2021-12-16 10:48:57 +0100

    traffic light

--- Password_checker/script.js
+++ Password_checker/script.js
@@ -1,16 +1,23 @@
+/* 1. Same litery i krotszy niz 8 znakow to obrazek i w paragrafie napis haslo slabe (na czerwono)
+    2. Same litery dluzsze niz 8 znakow napis haslo średnie (na żółto)
+    3. Haslo zawiera cyfre i jest dluzsze niz 8 znakow to haslo dobre 
+    4. Zawiera cyfre znak specjalny i jest dluzsze niz 8 znakow to haslo bardzo dobre
+*/
+
 var znaki = ["-","_","+","!","@","#","$","%","^","&","*","(",")","{","}","[","]","=","<",">","/"];
 var liczby = ["1","2","3","4","5","6","7","8","9","0"];
 
 function check(){
     var pswd = document.getElementById("wyraz").value;
     if(pswd.length <= 8){
-        document.getElementById("obraz").innerHTML = "<img src='images/cancel.png' alt='slabe'>";
+        clear();
+        document.getElementById("red").style.backgroundColor = "red";
         document.getElementById("wynik").innerHTML = "hasło <span style='color: red'>słabe</span>";
     }
     if(pswd.length > 8){
         var cyfra = false;
         var specjalny = false;
-        for(let znak of pswd){
+        for(znak of pswd){
             if(znaki.includes(znak)){
                 specjalny = true;
             }
@@ -19,16 +26,25 @@ function check(){
             }
         }
         if(cyfra === true){
-            document.getElementById("obraz").innerHTML = "<img src='images/check.png' alt='dobre'>";
+            clear();
+            document.getElementById("green").style.backgroundColor = "green";
             document.getElementById("wynik").innerHTML = "hasło <span style='color: chartreuse'>dobre</span>";
         }
         if(cyfra == true && specjalny == true){
-            document.getElementById("obraz").innerHTML = "<img src='images/padlock.png' alt='slabe'>";
+            clear();
+            document.getElementById("green").style.backgroundColor = "green";
             document.getElementById("wynik").innerHTML = "hasło <span style='color: green'>bardzo dobre</span>";
         }
         if(specjalny == false && cyfra == false){
-            document.getElementById("obraz").innerHTML = "<img src='images/check2.png' alt='sredni'>";
+            clear();
+            document.getElementById("yellow").style.backgroundColor = "yellow";
             document.getElementById("wynik").innerHTML = "hasło <span style='color: yellow'>średnie</span>";
         }
     }
+}
+
+function clear(){
+    document.getElementById("green").style.backgroundColor = "#111";
+    document.getElementById("yellow").style.backgroundColor = "#111";
+    document.getElementById("red").style.backgroundColor = "#111";
 }
