8eec703464e4ad7f2e10a6c2641908518668c485
Dejmenek <dejmen14567@gmail.com>
2022-02-01 13:41:16 +0100

    clock

---
+++ Zegar/script.js
@@ -0,0 +1,25 @@
+const hourHand = document.getElementById("hour_hand");
+const minuteHand = document.getElementById("minute_hand");
+const secondHand = document.getElementById("second_hand");
+
+
+function runClock(){
+    const date = new Date();
+
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    console.log(`Godzina: ${hour} Minuty: ${minute} Sekundy ${seconds}`);
+
+    let hourDegrees = ((hour * 360) / 12) + ((minute * 6) / 12);
+    let minuteDegrees = ((minute * 360) / 60) + ((seconds * 6) / 60);
+    let secondsDegrees = ((seconds * 360) / 60);
+
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+}
+
+setInterval(runClock,1000);
+runClock();
